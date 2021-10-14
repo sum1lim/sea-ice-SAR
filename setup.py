16b0c44f5f262de48e2033e2f5376d07d0c6111c
Sangwon Lim <angwonlim@gmail.com>
2021-10-13 18:03:37 -0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+__version__ = (0, 0, 0)
+
+setup(
+    name="sea_ice_SAR",
+    description="Deep Learning Architectures for Sea Ice Thickeness and Roughness Remote Sensing",
+    version=".".join(str(d) for d in __version__),
+    author="Sangwon Lim",
+    author_email="sangwonl@uvic.ca",
+    packages=["sea_ice_SAR"],
+    include_package_data=True,
+    scripts="""
+        ./scripts/
+    """.split(),
+)
