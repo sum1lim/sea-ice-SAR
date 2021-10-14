96ad2ec1370fb054ad1e9b5852048c330cdc4123
Sangwon Lim <angwonlim@gmail.com>
2021-10-14 16:57:56 -0700

    use gdal

--- utils/data_processing.py
+++ utils/data_processing.py
@@ -1,5 +1,8 @@
 import csv
+from osgeo import gdal
 
 
-def get_coordinates():
-    None
+def get_coordinates(te_data):
+    csv_reader = csv.reader(te_data)
+    for row in csv_reader:
+        print(row)
