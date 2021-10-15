30eccdfa5b7168fac6d9109da89325b477c84138
Sangwon Lim <angwonlim@gmail.com>
2021-10-15 16:07:35 -0700

    add get_pixel to utils

---
+++ sea_ice_SAR/utils.py
@@ -0,0 +1,39 @@
+from osgeo import osr
+
+wgs84_wkt = """
+GEOGCS["WGS 84",
+    DATUM["WGS_1984",
+        SPHEROID["WGS 84",6378137,298.257223563,
+            AUTHORITY["EPSG","7030"]],
+        AUTHORITY["EPSG","6326"]],
+    PRIMEM["Greenwich",0,
+        AUTHORITY["EPSG","8901"]],
+    UNIT["degree",0.01745329251994328,
+        AUTHORITY["EPSG","9122"]],
+    AUTHORITY["EPSG","4326"]]
+"""
+
+def get_pixel(ds, lat, lon):
+    dataset_cs= osr.SpatialReference()
+    dataset_cs.ImportFromWkt(ds.GetProjectionRef())
+
+    # create the new coordinate system
+    wgs84 = osr.SpatialReference()
+    wgs84.ImportFromWkt(wgs84_wkt)
+
+    # create a transform object to convert between coordinate systems
+    transform = osr.CoordinateTransformation(wgs84,dataset_cs) 
+
+    #get the coordinates in lat long
+    ds_coord = transform.TransformPoint(float(lat), float(lon)) 
+
+    xOrigin, xResolution, xRotation, yOrigin, yRotation, yResolution = ds.GetGeoTransform()
+    
+    # Note that the pixel/line coordinates in the above are from (0.0,0.0) at the top left corner of the top left pixel 
+    # to (width_in_pixels,height_in_pixels) at the bottom right corner of the bottom right pixel. 
+    # The pixel/line location of the center of the top left pixel would therefore be (0.5,0.5).
+    # https://gdal.org/user/raster_data_model.html#affine-geotransform
+    col = int((ds_coord[0] - xOrigin) / xResolution)
+    row = int((ds_coord[1] - yOrigin) / yResolution)
+
+    return row, col
