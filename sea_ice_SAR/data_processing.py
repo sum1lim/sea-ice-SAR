7fc1c49b73be865f0a70194b6c03f1610ab9c367
Sangwon Lim <angwonlim@gmail.com>
2021-10-19 12:33:01 -0700

    continue the loop instead of break

--- sea_ice_SAR/data_processing.py
+++ sea_ice_SAR/data_processing.py
@@ -11,7 +11,7 @@ def create_dataset(expert_data, features_dir):
     fature_li = ["label"]
     for iteration, ff in enumerate(features_files):
         if not ff.endswith(".tif"):
-            break
+            continue
 
         print(f"Reading {features_dir}/{ff}", file=sys.stdout)
         fature_li.append("".join(ff.split(".")[:-1]))
@@ -21,8 +21,10 @@ def create_dataset(expert_data, features_dir):
 
         if iteration == 0:
             pixels = {}
-            for datum in expert_data:
-                row, col = get_pixel(ds, datum[0], datum[1])
+            for idx, datum in enumerate(expert_data):
+                if "" in datum:
+                    continue
+                row, col = get_pixel(ds, datum[0], datum[1])                
                 if (row, col) not in pixels.keys():
                     pixels[(row, col)] = [[float(datum[2])], band_arr[row, col]]
                 else:
