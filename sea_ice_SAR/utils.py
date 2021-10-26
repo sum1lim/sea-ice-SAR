b248d4d58d8cb4092003f9b4c6b6f28199ebd27d
Sangwon Lim <angwonlim@gmail.com>
2021-10-25 18:45:12 -0700

    decompose_filepath

--- sea_ice_SAR/utils.py
+++ sea_ice_SAR/utils.py
@@ -46,3 +46,26 @@ def get_pixel(ds, lat, lon):
     row = int((ds_coord[1] - yOrigin) // yResolution)
 
     return row, col
+
+
+def decompose_filepath(filepath):
+    """
+    decompose filepath into three components:
+    directory path, file name and extension
+    """
+    parent_directories = filepath.split("/")[:-1]
+    try:
+        if parent_directories[-1] == "":
+            del parent_directories[-1]
+        dir_path = "/".join(parent_directories)
+    except IndexError:
+        dir_path = None
+
+    File = filepath.split("/")[-1]
+    try:
+        [filename, extension] = File.split(".")
+    except ValueError:
+        filename = File
+        extension = None
+
+    return (dir_path, filename, extension)
