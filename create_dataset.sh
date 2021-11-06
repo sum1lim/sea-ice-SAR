f96a6e07c0431a148fbf2b692d0fe5d9a120c937
Sangwon Lim <angwonlim@gmail.com>
2021-11-05 18:23:49 -0700

    CAE done

--- create_dataset.sh
+++ create_dataset.sh
@@ -1,17 +1,17 @@
-create_dataset --config ./Dataset_configs/roughness_c5.yml --window-size=9
-create_dataset --config ./Dataset_configs/thickness_c5.yml --window-size=9
+create_dataset --config ./Configs/Dataset_configs/roughness_c5.yml --window-size=9
+create_dataset --config ./Configs/Dataset_configs/thickness_c5.yml --window-size=9
 
-create_dataset --config ./Dataset_configs/roughness_hh.yml --window-size=9
-create_dataset --config ./Dataset_configs/thickness_hh.yml --window-size=9
+create_dataset --config ./Configs/Dataset_configs/roughness_hh.yml --window-size=9
+create_dataset --config ./Configs/Dataset_configs/thickness_hh.yml --window-size=9
 
-create_dataset --config ./Dataset_configs/roughness_hv.yml --window-size=9
-create_dataset --config ./Dataset_configs/thickness_hv.yml --window-size=9
+create_dataset --config ./Configs/Dataset_configs/roughness_hv.yml --window-size=9
+create_dataset --config ./Configs/Dataset_configs/thickness_hv.yml --window-size=9
 
-create_dataset --config ./Dataset_configs/roughness_hv_std5.yml --window-size=9
-create_dataset --config ./Dataset_configs/thickness_hv_std5.yml --window-size=9
+create_dataset --config ./Configs/Dataset_configs/roughness_hv_std5.yml --window-size=9
+create_dataset --config ./Configs/Dataset_configs/thickness_hv_std5.yml --window-size=9
 
-create_dataset --config ./Dataset_configs/roughness_hh_std5.yml --window-size=9
-create_dataset --config ./Dataset_configs/thickness_hh_std5.yml --window-size=9
+create_dataset --config ./Configs/Dataset_configs/roughness_hh_std5.yml --window-size=9
+create_dataset --config ./Configs/Dataset_configs/thickness_hh_std5.yml --window-size=9
 
-create_dataset --config ./Dataset_configs/roughness_hv_hh.yml --window-size=9
-create_dataset --config ./Dataset_configs/thickness_hv_hh.yml --window-size=9
+create_dataset --config ./Configs/Dataset_configs/roughness_hv_hh.yml --window-size=9
+create_dataset --config ./Configs/Dataset_configs/thickness_hv_hh.yml --window-size=9
