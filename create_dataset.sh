22983431f8b544d2689120246409be2300246f7e
Sangwon Lim <angwonlim@gmail.com>
2021-11-07 15:06:42 -0800

    update bash script

--- create_dataset.sh
+++ create_dataset.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
 
-create_dataset --config ./Configs/Dataset_configs/roughness_c5.yml 
-create_dataset --config ./Configs/Dataset_configs/thickness_c5.yml 
-
-create_dataset --config ./Configs/Dataset_configs/roughness_hh.yml 
-create_dataset --config ./Configs/Dataset_configs/thickness_hh.yml 
-
-create_dataset --config ./Configs/Dataset_configs/roughness_hv.yml 
-create_dataset --config ./Configs/Dataset_configs/thickness_hv.yml 
-
-create_dataset --config ./Configs/Dataset_configs/roughness_hv_std5.yml 
-create_dataset --config ./Configs/Dataset_configs/thickness_hv_std5.yml 
-
-create_dataset --config ./Configs/Dataset_configs/roughness_hh_std5.yml 
-create_dataset --config ./Configs/Dataset_configs/thickness_hh_std5.yml 
-
-create_dataset --config ./Configs/Dataset_configs/roughness_hv_hh.yml 
-create_dataset --config ./Configs/Dataset_configs/thickness_hv_hh.yml 
+create_dataset --config ./Configs/Dataset_configs/*.yml 
