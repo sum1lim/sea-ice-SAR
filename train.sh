32ac590a62fb9d8f96ac95b7bb3a81e57d51262e
Sangwon Lim <angwonlim@gmail.com>
2021-11-10 15:12:43 -0800

    updat train.sh

--- train.sh
+++ train.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-NN_regression --ml-config ./Configs/ML_configs/rms_regr* --result-dir ./results/NN_rms_regr
-test_regression --ml-config ./Configs/ML_configs/rms_regr* --result-dir ./results/NN_rms_regr
+NN_regression --ml-config ./Configs/ML_configs/* --result-dir ./results/NN_rms_regr
+test_regression --ml-config ./Configs/ML_configs/* --result-dir ./results/NN_rms_regr
 
-NN_regression --ml-config ./Configs/ML_configs/thk_regr* --result-dir ./results/NN_thk_regr
-test_regression --ml-config ./Configs/ML_configs/thk_regr* --result-dir ./results/NN_thk_regr
+NN_regression --ml-config ./Configs/ML_configs/* --result-dir ./results/NN_thk_regr
+test_regression --ml-config ./Configs/ML_configs/* --result-dir ./results/NN_thk_regr
 
-NN_classification --ml-config ./Configs/ML_configs/thk_class* --result-dir ./results/NN_thk_class
-test_classification --ml-config ./Configs/ML_configs/thk_class* --result-dir ./results/NN_thk_class
+NN_classification --ml-config ./Configs/ML_configs/* --result-dir ./results/NN_thk_class
+test_classification --ml-config ./Configs/ML_configs/* --result-dir ./results/NN_thk_class
