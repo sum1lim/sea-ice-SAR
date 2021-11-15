d84c0475189886dc7560cf098972362d73e43ba6
Sangwon Lim <angwonlim@gmail.com>
2021-11-14 21:44:17 -0800

    CAE with c7 and ratio

--- train.sh
+++ train.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-NN_regression --ml-config ./Configs/ML_configs/NN_rms_regr/* --result-dir ./results/NN_rms_regr
-test_regression --ml-config ./Configs/ML_configs/NN_rms_regr/* --result-dir ./results/NN_rms_regr
+for file in ./Configs/ML_configs/NN_rms_regr/*; do 
+    if [ -f "$file" ]; then
+        echo "************************* $file *************************" 
+        NN_regression --ml-config "$file" --result-dir ./results/NN_rms_regr
+        test_regression --ml-config "$file" --result-dir ./results/NN_rms_regr
+    fi
+done
 
-NN_regression --ml-config ./Configs/ML_configs/NN_thk_regr/* --result-dir ./results/NN_thk_regr
-test_regression --ml-config ./Configs/ML_configs/NN_thk_regr/* --result-dir ./results/NN_thk_regr
+for file in ./Configs/ML_configs/NN_thk_regr/*; do 
+    if [ -f "$file" ]; then
+        echo "************************* $file *************************" 
+        NN_regression --ml-config "$file" --result-dir ./results/NN_thk_regr
+        test_regression --ml-config "$file" --result-dir ./results/NN_thk_regr
+    fi
+done
 
-NN_classification --ml-config ./Configs/ML_configs/NN_thk_class/* --result-dir ./results/NN_thk_class
-test_classification --ml-config ./Configs/ML_configs/NN_thk_class/* --result-dir ./results/NN_thk_class
+for file in ./Configs/ML_configs/NN_thk_class/*; do 
+    if [ -f "$file" ]; then
+        echo "************************* $file *************************" 
+        NN_classification --ml-config "$file" --result-dir ./results/NN_thk_class
+        test_classification --ml-config "$file" --result-dir ./results/NN_thk_class
+    fi
+done
