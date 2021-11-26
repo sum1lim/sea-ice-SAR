9f68a92823e12b63d4dd0b5662d4888db7570781
Sangwon Lim <angwonlim@gmail.com>
2021-11-26 15:10:38 -0800

    update train.sh

--- train.sh
+++ train.sh
@@ -1,45 +1,39 @@
 #!/bin/bash
 
-for dir in ./Configs/ML_configs/NN_rms_regr/*; do 
-    if [ -d "$dir" ]; then
-        for file in "$dir"/*; do
-            if [ -f "$file" ]; then
-                echo "************************* $file *************************" 
-                NN_regression --ml-config "$file" --result-dir "$dir"
-                test_regression --ml-config "$file" --result-dir "$dir"
-            fi
-        done
-    fi
+for dir in FYI FYI_MYI MYI; do 
+    for file in ./Configs/ML_configs/NN_rms_regr/"$dir"/*; do
+        if [ -f "$file" ]; then
+            echo "************************* $file *************************" 
+            NN_regression --ml-config "$file" --result-dir ./results/NN_rms_regr/"$dir"
+            test_regression --ml-config "$file" --result-dir ./results/NN_rms_regr/"$dir"/
+        fi
+    done
 done
 
-for dir in ./Configs/ML_configs/NN_thk_regr/*; do 
-    if [ -d "$dir" ]; then
-        for file in "$dir"/*; do
-            if [ -f "$file" ]; then
-                echo "************************* $file *************************" 
-                NN_regression --ml-config "$file" --result-dir "$dir"
-                test_regression --ml-config "$file" --result-dir "$dir"
-            fi
-        done
-    fi
+for dir in FYI FYI_MYI MYI; do 
+    for file in ./Configs/ML_configs/NN_thk_regr/"$dir"/*; do
+        if [ -f "$file" ]; then
+            echo "************************* $file *************************" 
+            NN_regression --ml-config "$file" --result-dir ./results/NN_thk_regr/"$dir"
+            test_regression --ml-config "$file" --result-dir ./results/NN_thk_regr/"$dir"/
+        fi
+    done
 done
 
-for dir in ./Configs/ML_configs/NN_thk_class/*; do 
-    if [ -d "$dir" ]; then
-        for file in "$dir"/*; do
-            if [ -f "$file" ]; then
-                echo "************************* $file *************************" 
-                NN_classification --ml-config "$file" --result-dir "$dir"
-                test_classification --ml-config "$file" --result-dir "$dir"
-            fi
-        done
-    fi
+for dir in FYI FYI_MYI MYI; do 
+    for file in ./Configs/ML_configs/NN_thk_class/"$dir"/*; do
+        if [ -f "$file" ]; then
+            echo "************************* $file *************************" 
+            NN_classification --ml-config "$file" --result-dir ./results/NN_thk_class/"$dir"
+            NN_classification --ml-config "$file" --result-dir ./results/NN_thk_class/"$dir"/
+        fi
+    done
 done
 
 for file in ./Configs/ML_configs/NN_type_class/*; do 
     if [ -f "$file" ]; then
         echo "************************* $file *************************" 
-        NN_classification --ml-config "$file" --result-dir ./Configs/ML_configs/NN_type_class
-        test_classification --ml-config "$file" --result-dir ./Configs/ML_configs/NN_type_class
+        NN_classification --ml-config "$file" --result-dir ./results/NN_type_class
+        test_classification --ml-config "$file" --result-dir ./results/NN_type_class
     fi
 done
