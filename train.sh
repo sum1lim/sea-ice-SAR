5418e35129076016fd1ad661c46c8b64a1db0f2c
Sangwon Lim <angwonlim@gmail.com>
2021-12-01 09:48:17 -0800

    update train.sh

--- train.sh
+++ train.sh
@@ -4,20 +4,20 @@ for parent_dir in init SMOTE; do
         for file in ./Configs/ML_configs/NN_rms_regr/"$parent_dir"/"$child_dir"/*; do
             if [ -f "$file" ]; then
                 echo "************************* $file *************************" 
-                NN_regression --ml-config "$file" --result-dir ./results/NN_rms_regr/"$dir"
-                test_regression --ml-config "$file" --result-dir ./results/NN_rms_regr/"$dir"
+                NN_regression --ml-config "$file" --result-dir ./results/NN_rms_regr/"$parent_dir"/"$child_dir"
+                test_regression --ml-config "$file" --result-dir ./results/NN_rms_regr/"$parent_dir"/"$child_dir"
             fi
         done
     done
 done
 
 for parent_dir in init SMOTE; do 
-    for dir in FYI FYI_MYI MYI; do 
+    for child_dir in FYI FYI_MYI MYI; do 
         for file in ./Configs/ML_configs/NN_thk_regr/"$parent_dir"/"$child_dir"/*; do
             if [ -f "$file" ]; then
                 echo "************************* $file *************************" 
-                NN_regression --ml-config "$file" --result-dir ./results/NN_thk_regr/"$dir"
-                test_regression --ml-config "$file" --result-dir ./results/NN_thk_regr/"$dir"
+                NN_regression --ml-config "$file" --result-dir ./results/NN_thk_regr/"$parent_dir"/"$child_dir"
+                test_regression --ml-config "$file" --result-dir ./results/NN_thk_regr/"$parent_dir"/"$child_dir"
             fi
         done
     done
