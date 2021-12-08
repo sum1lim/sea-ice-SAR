952eca2f56a99a3f34f0e6d071363ac2797e4202
Sangwon Lim <angwonlim@gmail.com>
2021-12-07 17:35:09 -0800

    update the repo

--- train.sh
+++ train.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
-for parent_dir in init window; do 
-    for child_dir in FYI FYI_MYI MYI; do 
-        for file in ./Configs/ML_configs/NN_rms_regr/"$parent_dir"/"$child_dir"/*; do
-            if [ -f "$file" ]; then
-                echo "************************* $file *************************" 
-                NN_regression --ml-config "$file" --result-dir ./results/NN_rms_regr/"$parent_dir"/"$child_dir"
-                test_regression --ml-config "$file" --result-dir ./results/NN_rms_regr/"$parent_dir"/"$child_dir"
-            fi
-        done
+
+for parent_dir in FYI FYI_MYI MYI; do 
+    for file in ./Configs/ML_configs/NN_rms_regr/"$parent_dir"/*; do
+        if [ -f "$file" ]; then
+            echo "************************* $file *************************" 
+            NN_regression --ml-config "$file" --result-dir ./results/NN_rms_regr/"$parent_dir"
+            test_regression --ml-config "$file" --result-dir ./results/NN_rms_regr/"$parent_dir"
+        fi
     done
 done
 
-for parent_dir in init window; do 
-    for child_dir in FYI FYI_MYI MYI; do 
-        for file in ./Configs/ML_configs/NN_thk_regr/"$parent_dir"/"$child_dir"/*; do
-            if [ -f "$file" ]; then
-                echo "************************* $file *************************" 
-                NN_regression --ml-config "$file" --result-dir ./results/NN_thk_regr/"$parent_dir"/"$child_dir"
-                test_regression --ml-config "$file" --result-dir ./results/NN_thk_regr/"$parent_dir"/"$child_dir"
-            fi
-        done
+for parent_dir in FYI FYI_MYI MYI; do 
+    for file in ./Configs/ML_configs/NN_thk_regr/"$parent_dir"/*; do
+        if [ -f "$file" ]; then
+            echo "************************* $file *************************" 
+            NN_regression --ml-config "$file" --result-dir ./results/NN_thk_regr/"$parent_dir"/"$child_dir"
+            test_regression --ml-config "$file" --result-dir ./results/NN_thk_regr/"$parent_dir"/"$child_dir"
+        fi
     done
 done
 
