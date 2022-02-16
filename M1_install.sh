fa7e736669ce1383e3afa27d888f892fae796f84
Sangwon Lim <angwonlim@gmail.com>
2022-02-16 10:13:18 -0800

    install of base + conda env naming

--- M1_install.sh
+++ M1_install.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
+rm -rf ~/sis-venv
+
 curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-MacOSX-arm64.sh" --output ./Miniforge3-MacOSX-arm64.sh
 chmod +x ./Miniforge3-MacOSX-arm64.sh
 sh ./Miniforge3-MacOSX-arm64.sh
+source ~/miniforge3/bin/activate
 rm ./Miniforge3-MacOSX-arm64.sh
-
-conda remove -n sis-venv
-conda create --name=sis-venv
-conda activate sis-venv
 
 python3 -m pip install --upgrade pip
 
@@ -16,4 +15,6 @@ pip uninstall tensorflow-metal
 conda install -c apple tensorflow-deps --force-reinstall
 
 pip install -r M1_requirements.txt
-pip install -e .
+pip install .
+
+mv ~/miniforge3 ~/sis-venv
