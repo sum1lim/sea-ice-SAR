aa9e48aaacb7867a16e125fa1906fd10fcac62fe
Sangwon Lim <angwonlim@gmail.com>
2022-02-15 17:21:10 -0800

    stable M1 build

---
+++ M1_install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-MacOSX-arm64.sh" --output ./Miniforge3-MacOSX-arm64.sh
+chmod +x ./Miniforge3-MacOSX-arm64.sh
+sh ./Miniforge3-MacOSX-arm64.sh
+rm ./Miniforge3-MacOSX-arm64.sh
+
+conda remove -n sis-venv
+conda create --name=sis-venv
+conda activate sis-venv
+
+python3 -m pip install --upgrade pip
+
+pip uninstall tensorflow-macos
+pip uninstall tensorflow-metal
+conda install h5py --force-reinstall
+conda install -c apple tensorflow-deps --force-reinstall
+
+pip install -r M1_requirements.txt
+pip install -e .
