49b24a427c2b315d88dd27eb1b60b66856da86d5
Sangwon Lim <angwonlim@gmail.com>
2022-02-16 10:27:41 -0800

    install on base

--- M1_install.sh
+++ M1_install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-rm -rf ~/sis-venv
-
 curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-MacOSX-arm64.sh" --output ./Miniforge3-MacOSX-arm64.sh
 chmod +x ./Miniforge3-MacOSX-arm64.sh
 sh ./Miniforge3-MacOSX-arm64.sh
@@ -15,6 +13,4 @@ pip uninstall tensorflow-metal
 conda install -c apple tensorflow-deps --force-reinstall
 
 pip install -r M1_requirements.txt
-pip install .
-
-mv ~/miniforge3 ~/sis-venv
+pip install -e .
