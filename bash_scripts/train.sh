86937be3a74a6f9d20e8cc919a356b6928e96f3d
Sangwon Lim <angwonlim@gmail.com>
2022-03-17 12:21:45 -0700

    update CAE models

--- bash_scripts/train.sh
+++ bash_scripts/train.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-for file in backscatter_spk backscatter CAE_spk CAE; do
+for file in backscatter_spk backscatter CAE_spk CAE mixed; do
     echo "************************* NN_type_class/$file *************************" 
-    NN_classification --ml-config ./Configs/ML_configs/NN_type_class/"$file" --result-dir ./results/NN_type_class
-    test_classification --ml-config ./Configs/ML_configs/NN_type_class/"$file" --result-dir ./results/NN_type_class
+    NN_classification --ml-config ./Configs/ML_configs/NN_type_class/"$file".yml --result-dir ./results/NN_type_class
+    test_classification --ml-config ./Configs/ML_configs/NN_type_class/"$file".yml --result-dir ./results/NN_type_class
 done
-boxplot --result-dir ./results/NN_type_class/ --model-type classification --methods backscatter_spk backscatter CAE_spk CAE
+boxplot --result-dir ./results/NN_type_class/ --model-type classification --methods backscatter_spk backscatter CAE_spk CAE mixed
 
 for parent_dir in FYI_MYI; do 
-    for file in backscatter_spk backscatter CAE_spk CAE; do
+    for file in backscatter_spk backscatter CAE_spk CAE mixed; do
         echo "************************* NN_rms_regr/$parent_dir/$file *************************" 
         NN_regression --ml-config ./Configs/ML_configs/NN_rms_regr/"$parent_dir"/"$file".yml --result-dir ./results/NN_rms_regr/"$parent_dir"
         test_regression --ml-config ./Configs/ML_configs/NN_rms_regr/"$parent_dir"/"$file".yml --result-dir ./results/NN_rms_regr/"$parent_dir"
     done
-    boxplot --result-dir ./results/NN_rms_regr/"$parent_dir" --model-type regression --methods backscatter_spk backscatter CAE_spk CAE
+    boxplot --result-dir ./results/NN_rms_regr/"$parent_dir" --model-type regression --methods backscatter_spk backscatter CAE_spk CAE mixed
 done
 
 for parent_dir in FYI_MYI; do 
-    for file in backscatter_spk backscatter CAE_spk CAE; do
+    for file in backscatter_spk backscatter CAE_spk CAE mixed; do
         echo "************************* NN_thk_regr/$parent_dir/$file *************************" 
         NN_regression --ml-config ./Configs/ML_configs/NN_thk_regr/"$parent_dir"/"$file".yml --result-dir ./results/NN_thk_regr/"$parent_dir"/"$child_dir"
         test_regression --ml-config ./Configs/ML_configs/NN_thk_regr/"$parent_dir"/"$file".yml --result-dir ./results/NN_thk_regr/"$parent_dir"/"$child_dir"
     done
-    boxplot --result-dir ./results/NN_thk_regr/"$parent_dir" --model-type regression --methods backscatter_spk backscatter CAE_spk CAE
+    boxplot --result-dir ./results/NN_thk_regr/"$parent_dir" --model-type regression --methods backscatter_spk backscatter CAE_spk CAE mixed
 done
