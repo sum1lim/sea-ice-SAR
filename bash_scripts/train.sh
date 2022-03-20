73fd4b1c1479d7702d5a6d6916f9cda50bea2560
Sangwon Lim <angwonlim@gmail.com>
2022-03-20 10:56:14 -0700

    update train.sh

--- bash_scripts/train.sh
+++ bash_scripts/train.sh
@@ -7,8 +7,8 @@ for file in CAE backscatter; do
 done
 boxplot --result-dir ./results/NN_type_class/ --model-type classification --methods CAE backscatter
 
-for parent_dir in FYI_MYI; do 
-    for file in CAE backscatter; do
+for parent_dir in FYI_MYI_likelihood FYI_MYI; do 
+    for file in CAE backscatter CNN; do
         echo "************************* NN_rms_regr/$parent_dir/$file *************************" 
         NN_regression --ml-config ./Configs/ML_configs/NN_rms_regr/"$parent_dir"/"$file".yml --result-dir ./results/NN_rms_regr/"$parent_dir"
         test_regression --ml-config ./Configs/ML_configs/NN_rms_regr/"$parent_dir"/"$file".yml --result-dir ./results/NN_rms_regr/"$parent_dir"
@@ -16,8 +16,8 @@ for parent_dir in FYI_MYI; do
     boxplot --result-dir ./results/NN_rms_regr/"$parent_dir" --model-type regression --methods CAE backscatter
 done
 
-for parent_dir in FYI_MYI; do 
-    for file in CAE backscatter; do
+for parent_dir in FYI_MYI_likelihood FYI_MYI; do 
+    for file in CAE backscatter CNN; do
         echo "************************* NN_thk_regr/$parent_dir/$file *************************" 
         NN_regression --ml-config ./Configs/ML_configs/NN_thk_regr/"$parent_dir"/"$file".yml --result-dir ./results/NN_thk_regr/"$parent_dir"/"$child_dir"
         test_regression --ml-config ./Configs/ML_configs/NN_thk_regr/"$parent_dir"/"$file".yml --result-dir ./results/NN_thk_regr/"$parent_dir"/"$child_dir"
