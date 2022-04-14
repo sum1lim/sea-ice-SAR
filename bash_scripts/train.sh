d7586d1aee40f5be2db16d819bd3adef9644504a
Sangwon Lim <angwonlim@gmail.com>
2022-04-13 22:16:22 -0700

    update configs and train.sh

--- bash_scripts/train.sh
+++ bash_scripts/train.sh
@@ -7,20 +7,17 @@ for file in CNN CNN_spk CAE CAE_spk backscatter; do
 done
 boxplot --result-dir ./results/NN_type_class/ --model-type classification --methods CNN CNN_spk CAE CAE_spk backscatter
 
-for parent_dir in FYI_MYI; do 
-    for file in CNN CNN_spk CAE CAE_spk backscatter; do
-        echo "************************* NN_thk_regr/$parent_dir/$file *************************" 
-        NN_regression --ml-config ./Configs/ML_configs/NN_thk_regr/"$parent_dir"/"$file".yml --result-dir ./results/NN_thk_regr/"$parent_dir"/"$file"
-        test_regression --ml-config ./Configs/ML_configs/NN_thk_regr/"$parent_dir"/"$file".yml --result-dir ./results/NN_thk_regr/"$parent_dir"/"$file"
-    done
-    boxplot --result-dir ./results/NN_thk_regr/"$parent_dir" --model-type regression --methods CNN CNN_spk CAE CAE_spk backscatter
+for file in FYI_MYI FYI_MYI_likelihood; do
+    echo "************************* NN_thk_regr/$file *************************" 
+    NN_regression --ml-config ./Configs/ML_configs/NN_thk_regr/"$file".yml --result-dir ./results/NN_thk_regr/"$file"
+    test_regression --ml-config ./Configs/ML_configs/NN_thk_regr/"$file".yml --result-dir ./results/NN_thk_regr/"$file"
 done
+boxplot --result-dir ./results/NN_thk_regr --model-type regression --methods FYI_MYI FYI_MYI_likelihood
 
-for parent_dir in FYI_MYI; do 
-    for file in CNN CNN_spk CAE CAE_spk backscatter; do
-        echo "************************* NN_rms_regr/$parent_dir/$file *************************" 
-        NN_regression --ml-config ./Configs/ML_configs/NN_rms_regr/"$parent_dir"/"$file".yml --result-dir ./results/NN_rms_regr/"$parent_dir"/"$file"
-        test_regression --ml-config ./Configs/ML_configs/NN_rms_regr/"$parent_dir"/"$file".yml --result-dir ./results/NN_rms_regr/"$parent_dir"/"$file"
-    done
-    boxplot --result-dir ./results/NN_rms_regr/"$parent_dir" --model-type regression --methods CNN CNN_spk CAE CAE_spk backscatter
+for file in FYI_MYI FYI_MYI_likelihood; do
+    echo "************************* NN_rms_regr/$file *************************" 
+    NN_regression --ml-config ./Configs/ML_configs/NN_rms_regr/"$file".yml --result-dir ./results/NN_rms_regr/"$file"
+    test_regression --ml-config ./Configs/ML_configs/NN_rms_regr/"$file".yml --result-dir ./results/NN_rms_regr/"$file"
 done
+boxplot --result-dir ./results/NN_rms_regr --model-type regression --methods FYI_MYI FYI_MYI_likelihood
+
