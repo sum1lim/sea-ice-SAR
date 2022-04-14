c227103170b3c1d67901b8c9a2c55004e66cfad7
Sangwon Lim <angwonlim@gmail.com>
2022-04-13 22:07:31 -0700

    modify RNN.sh

--- bash_scripts/RNN.sh
+++ bash_scripts/RNN.sh
@@ -10,12 +10,12 @@ for i in {1..100}; do
     echo "************************* RNN_thk_$i *************************" 
     NN_regression --ml-config ./Configs/ML_configs/NN_thk_regr/RNN/recursion.yml --result-dir ./results/NN_thk_regr/RNN/recursion_"$i"
     test_regression --ml-config ./Configs/ML_configs/NN_thk_regr/RNN/recursion.yml --result-dir ./results/NN_thk_regr/RNN/recursion_"$i"
-    predict --ml-config ./Configs/ML_configs/NN_thk_regr/RNN/recursion.yml --result-dir ./results/NN_thk_regr/RNN/recursion_"$i" --checkpoint 1 --classes thk_"$(expr $i % 5)" --output-dir ./results/NN_thk_regr/RNN
-    predict --ml-config ./Configs/ML_configs/NN_thk_regr/RNN/pred.yml --result-dir ./results/NN_thk_regr/RNN/recursion_"$i" --checkpoint 1 --classes thk_"$(expr $i % 5)" --output-dir ./results/NN_thk_regr/RNN
+    predict --ml-config ./Configs/ML_configs/NN_thk_regr/RNN/recursion.yml --result-dir ./results/NN_thk_regr/RNN/recursion_"$i" --checkpoint 1 --classes thk_"$i" --output-dir ./results/NN_thk_regr/RNN
+    predict --ml-config ./Configs/ML_configs/NN_thk_regr/RNN/pred.yml --result-dir ./results/NN_thk_regr/RNN/recursion_"$i" --checkpoint 1 --classes thk_"$i" --output-dir ./results/NN_thk_regr/RNN
 
     echo "************************* RNN_rms_$i *************************" 
     NN_regression --ml-config ./Configs/ML_configs/NN_rms_regr/RNN/recursion.yml --result-dir ./results/NN_rms_regr/RNN/recursion_"$i"
     test_regression --ml-config ./Configs/ML_configs/NN_rms_regr/RNN/recursion.yml --result-dir ./results/NN_rms_regr/RNN/recursion_"$i"
-    predict --ml-config ./Configs/ML_configs/NN_rms_regr/RNN/recursion.yml --result-dir ./results/NN_rms_regr/RNN/recursion_"$i" --checkpoint 1 --classes rms_"$(expr $i % 5)" --output-dir ./results/NN_rms_regr/RNN
-    predict --ml-config ./Configs/ML_configs/NN_rms_regr/RNN/pred.yml --result-dir ./results/NN_rms_regr/RNN/recursion_"$i" --checkpoint 1 --classes rms_"$(expr $i % 5)" --output-dir ./results/NN_rms_regr/RNN
+    predict --ml-config ./Configs/ML_configs/NN_rms_regr/RNN/recursion.yml --result-dir ./results/NN_rms_regr/RNN/recursion_"$i" --checkpoint 1 --classes rms_"$i" --output-dir ./results/NN_rms_regr/RNN
+    predict --ml-config ./Configs/ML_configs/NN_rms_regr/RNN/pred.yml --result-dir ./results/NN_rms_regr/RNN/recursion_"$i" --checkpoint 1 --classes rms_"$i" --output-dir ./results/NN_rms_regr/RNN
 done
